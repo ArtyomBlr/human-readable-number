1298a2f9e612195d48000eeb00f672e86f2ae81c
ArtyomBlr <59910600+ArtyomBlr@users.noreply.github.com>
2020-02-26 20:31:08 +0300

    docs: add arrays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function toReadable (number) {
-  
+  var a = [
+  'zero', 
+  'one', 
+  'two', 
+  'three', 
+  'four', 
+  'five', 
+  'six', 
+  'seven', 
+  'eight', 
+  'nine',   
+]
+
+var c = [
+    'eleven', 
+  'twelve', 
+  'thirteen', 
+  'fourteen', 
+  'fifteen', 
+  'sixteen', 
+  'seventeen', 
+  'eighteen', 
+  'nineteen'
+]
+
+var b = [
+    'ten', 
+    'twenty', 
+    'thirty', 
+    'forty', 
+    'fifty', 
+    'sixty', 
+    'seventy', 
+    'eighty', 
+    'hundred'
+]
+
+    if(number >= 0 && number <= 9) {
+        return a[number];
+    } else if (number >= 11 &&  number <= 19) {
+        return c[number];
+    } else if (number)
 }
